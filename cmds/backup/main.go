30109d09554c32623b277e9956683611a095cbee
mihiro <f.mihiroxx@gmail.com>
2019-10-26 20:59:49 +0900

    Create main.go

---
+++ cmds/backup/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+
+	"github.com/matryer/filedb"
+)
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		dbpath = flag.String("db", "./backupdata", "データベースのディレクトリへのパス")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("エラー： コマンドを指定してください")
+		return
+	}
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+}
