e060a539e5afd23dc595377285db293a53935c15
mihiro <f.mihiroxx@gmail.com>
2019-10-26 20:30:38 +0900

    Add act func to monitor.go

--- monitor.go
+++ monitor.go
@@ -1,6 +1,12 @@
 package backup
 
-type monitor struct {
+import (
+	"fmt"
+	"path/filepath"
+	"time"
+)
+
+type Monitor struct {
 	Paths       map[string]string
 	Archiver    Archiver
 	Destination string
@@ -23,4 +29,10 @@ func (m *Monitor) Now() (int, error) {
 		}
 	}
 	return counter, nil
+}
+
+func (m *Monitor) act(path string) error {
+	dirname := filepath.Base(path)
+	filename := fmt.Sprintf("%d.zip", time.Now().UnixNano())
+	return m.Archiver.Archive(path, filepath.Join(m.Destination, dirname, filename))
 }
